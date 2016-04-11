6f1a731a7306004b7f199d3d53fd99ec0b846bd0
Lee Hicks <leehicks@dreamfactory.com>
2016-04-11 10:16:42 -0400

    enhancing schema extension

--- src/Database/Schema/Schema.php
+++ src/Database/Schema/Schema.php
@@ -3,6 +3,7 @@ namespace DreamFactory\Core\MongoDb\Database\Schema;
 
 use DreamFactory\Core\Database\ColumnSchema;
 use DreamFactory\Core\Database\TableSchema;
+use Jenssegers\Mongodb\Connection;
 use MongoDB\Model\CollectionInfo;
 
 /**
@@ -10,6 +11,11 @@ use MongoDB\Model\CollectionInfo;
  */
 class Schema extends \DreamFactory\Core\Database\Schema\Schema
 {
+    /**
+     * @var Connection
+     */
+    protected $connection;
+
     /**
      * @return boolean
      */
@@ -31,8 +37,26 @@ class Schema extends \DreamFactory\Core\Database\Schema\Schema
      */
     protected function loadTable(TableSchema $table)
     {
-        $c = new ColumnSchema(['name' => '_id', 'isPrimaryKey' => true, 'autoIncrement' => true]);
-        $table->addColumn($c);
+        if (null !== $template = $this->connection->table($table->name)->first()) {
+            foreach ($template as $name => $value) {
+                $c = new ColumnSchema(['name' => $name, 'allowNull' => true, 'type' => gettype($value)]);
+                if ('_id' === $name) {
+                    $c->isPrimaryKey = true;
+                    $c->autoIncrement = true;
+                    $c->allowNull = false;
+                }
+                $table->addColumn($c);
+            }
+        } else {
+            $c =
+                new ColumnSchema([
+                    'name'          => '_id',
+                    'type'          => 'string',
+                    'isPrimaryKey'  => true,
+                    'autoIncrement' => true
+                ]);
+            $table->addColumn($c);
+        }
 
         return $table;
     }
@@ -51,7 +75,7 @@ class Schema extends \DreamFactory\Core\Database\Schema\Schema
         $tables = [];
         /** @type \MongoDB\Database $db */
         /** @noinspection PhpUndefinedMethodInspection */
-        $db = $this->connection->getMongoDb();
+        $db = $this->connection->getMongoDB();
         $schema = $db->getDatabaseName(); // only one supported currently
         /** @type CollectionInfo[] $collections */
         $collections = $db->listCollections();
@@ -66,6 +90,180 @@ class Schema extends \DreamFactory\Core\Database\Schema\Schema
         }
 
         return $tables;
+    }
+
+    public function createTable($table, $schema, $options = null)
+    {
+        if (empty($schema['field'])) {
+            throw new \Exception("No valid fields exist in the received table schema.");
+        }
+
+        $results = $this->buildTableFields($table, $schema['field']);
+        if (empty($results['columns'])) {
+            throw new \Exception("No valid fields exist in the received table schema.");
+        }
+
+        $cols = [];
+        foreach ($results['columns'] as $name => $type) {
+            if (is_string($name)) {
+                $cols[] = "\t" . $this->quoteColumnName($name) . ' ' . $this->getColumnType($type);
+            } else {
+                $cols[] = "\t" . $type;
+            }
+        }
+
+        $this->connection->getMongoDB()->createCollection($table, $options);
+
+        return $results;
+    }
+
+    /**
+     * @param string $table_name
+     * @param array  $schema
+     * @param bool   $allow_delete
+     *
+     * @throws \Exception
+     * @return array
+     */
+    protected function updateTable($table_name, $schema, $allow_delete = false)
+    {
+        if (empty($table_name)) {
+            throw new \Exception("Table schema received does not have a valid name.");
+        }
+
+        // does it already exist
+        if (!$this->doesTableExist($table_name)) {
+            throw new \Exception("Update schema called on a table with name '$table_name' that does not exist in the database.");
+        }
+
+        //  Is there a name update
+        if (!empty($schema['new_name'])) {
+            // todo change table name, has issue with references
+        }
+
+        $oldSchema = $this->getTable($table_name);
+
+        // update column types
+
+        $results = [];
+        if (!empty($schema['field'])) {
+            $results =
+                $this->buildTableFields($table_name, $schema['field'], $oldSchema, true, $allow_delete);
+            if (isset($results['columns']) && is_array($results['columns'])) {
+                foreach ($results['columns'] as $name => $definition) {
+//                    $this->connection->statement($this->addColumn($table_name, $name, $definition));
+                }
+            }
+            if (isset($results['alter_columns']) && is_array($results['alter_columns'])) {
+                foreach ($results['alter_columns'] as $name => $definition) {
+//                    $this->connection->statement($this->alterColumn($table_name, $name, $definition));
+                }
+            }
+            if (isset($results['drop_columns']) && is_array($results['drop_columns'])) {
+                foreach ($results['drop_columns'] as $name) {
+//                    $this->connection->statement($this->dropColumn($table_name, $name));
+                }
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Builds and executes a SQL statement for dropping a DB table.
+     *
+     * @param string $table the table to be dropped. The name will be properly quoted by the method.
+     *
+     * @return integer 0 is always returned. See {@link http://php.net/manual/en/pdostatement.rowcount.php} for more
+     *                 information.
+     */
+    public function dropTable($table)
+    {
+        $this->connection->getCollection($table)->drop();
+        $this->removeSchemaExtrasForTables($table);
+
+        //  Any changes here should refresh cached schema
+        $this->refresh();
+
+        return ['name' => $table];
+    }
+
+    public function dropColumn($table, $column)
+    {
+        $result = 0;
+        $tableInfo = $this->getTable($table);
+        if (($columnInfo = $tableInfo->getColumn($column)) && (ColumnSchema::TYPE_VIRTUAL !== $columnInfo->type)) {
+        }
+        $this->removeSchemaExtrasForFields($table, $column);
+
+        //  Any changes here should refresh cached schema
+        $this->refresh();
+
+        return $result;
+    }
+
+    /**
+     * @param string $table_name
+     * @param array  $fields
+     * @param bool   $allow_update
+     * @param bool   $allow_delete
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function updateFields($table_name, $fields, $allow_update = false, $allow_delete = false)
+    {
+        if (empty($table_name)) {
+            throw new \Exception("Table schema received does not have a valid name.");
+        }
+
+        // does it already exist
+        if (!$this->doesTableExist($table_name)) {
+            throw new \Exception("Update schema called on a table with name '$table_name' that does not exist in the database.");
+        }
+
+        $oldSchema = $this->getTable($table_name);
+
+        $names = [];
+        $results = $this->buildTableFields($table_name, $fields, $oldSchema, $allow_update, $allow_delete);
+        if (isset($results['columns']) && is_array($results['columns'])) {
+            foreach ($results['columns'] as $name => $definition) {
+//                $this->connection->statement($this->addColumn($table_name, $name, $definition));
+                $names[] = $name;
+            }
+        }
+        if (isset($results['alter_columns']) && is_array($results['alter_columns'])) {
+            foreach ($results['alter_columns'] as $name => $definition) {
+//                $this->connection->statement($this->alterColumn($table_name, $name, $definition));
+                $names[] = $name;
+            }
+        }
+        if (isset($results['drop_columns']) && is_array($results['drop_columns'])) {
+            foreach ($results['drop_columns'] as $name) {
+//                $this->connection->statement($this->dropColumn($table_name, $name));
+                $names[] = $name;
+            }
+        }
+
+        $references = (isset($results['references'])) ? $results['references'] : [];
+        $this->createFieldReferences($references);
+
+        $indexes = (isset($results['indexes'])) ? $results['indexes'] : [];
+        $this->createFieldIndexes($indexes);
+
+        $extras = (isset($results['extras'])) ? $results['extras'] : [];
+        if (!empty($extras)) {
+            $this->setSchemaFieldExtras($extras);
+        }
+
+        $extras = (isset($results['drop_extras'])) ? $results['drop_extras'] : [];
+        if (!empty($extras)) {
+            foreach ($extras as $table => $dropFields) {
+                $this->removeSchemaExtrasForFields($table, $dropFields);
+            }
+        }
+
+        return ['names' => $names];
     }
 
     public function parseValueForSet($value, $field_info)
--- src/Resources/Schema.php
+++ src/Resources/Schema.php
@@ -1,19 +1,22 @@
 <?php
 namespace DreamFactory\Core\MongoDb\Resources;
 
+use DreamFactory\Core\Database\ColumnSchema;
 use DreamFactory\Core\Exceptions\BadRequestException;
 use DreamFactory\Core\Exceptions\InternalServerErrorException;
 use DreamFactory\Core\Exceptions\NotFoundException;
 use DreamFactory\Core\Exceptions\RestException;
+use DreamFactory\Core\MongoDb\Components\MongoDbResource;
 use DreamFactory\Core\Resources\BaseNoSqlDbSchemaResource;
 use DreamFactory\Core\MongoDb\Services\MongoDb;
-use MongoDB\Collection;
 
 class Schema extends BaseNoSqlDbSchemaResource
 {
     //*************************************************************************
     //	Members
     //*************************************************************************
+
+    use MongoDbResource;
 
     /**
      * @var null|MongoDb
@@ -25,34 +28,17 @@ class Schema extends BaseNoSqlDbSchemaResource
     //*************************************************************************
 
     /**
-     * @return null|MongoDb
-     */
-    public function getService()
-    {
-        return $this->parent;
-    }
-
-    /**
-     * @param $name
-     *
-     * @return Collection|null
-     */
-    public function selectTable($name)
-    {
-        $coll = $this->parent->getConnection()->getCollection($name);
-
-        return $coll;
-    }
-
-    /**
      * {@inheritdoc}
      */
-    public function describeTable($table, $refresh = true)
+    public function describeTable($name, $refresh = false)
     {
-        $name = (is_array($table)) ? array_get($table, 'name') : $table;
+        $name = (is_array($name) ? array_get($name, 'name') :  $name);
+        if (empty($name)) {
+            throw new BadRequestException('Table name can not be empty.');
+        }
 
         try {
-            $table = $this->parent->getSchema()->getTable($name, $refresh);
+            $table = $this->schema->getTable($name, $refresh);
             if (!$table) {
                 throw new NotFoundException("Table '$name' does not exist in the database.");
             }
@@ -71,19 +57,60 @@ class Schema extends BaseNoSqlDbSchemaResource
     /**
      * {@inheritdoc}
      */
-    public function createTable($table, $properties = [], $check_exist = false, $return_schema = false)
+    public function describeField($table, $field, $refresh = false)
     {
         if (empty($table)) {
-            throw new BadRequestException("No 'name' field in data.");
+            throw new BadRequestException('Table name can not be empty.');
         }
-
-        $properties = (is_array($properties) ? $properties : []);
 
         try {
-            $this->parent->getConnection()->getMongoDB()->createCollection($table, $properties);
+            $result = $this->describeTableFields($table, $field);
+
+            return array_get($result, 0);
+        } catch (RestException $ex) {
+            throw $ex;
         } catch (\Exception $ex) {
-            throw new InternalServerErrorException("Failed to create table '$table'.\n{$ex->getMessage()}");
+            throw new InternalServerErrorException("Error describing database table '$table' field '$field'.\n" .
+                $ex->getMessage(), $ex->getCode());
         }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createTables($tables, $check_exist = false, $return_schema = false)
+    {
+        $tables = static::validateAsArray($tables, null, true, 'There are no table sets in the request.');
+
+        foreach ($tables as $table) {
+            if (null === ($name = array_get($table, 'name'))) {
+                throw new BadRequestException("Table schema received does not have a valid name.");
+            }
+        }
+
+        $result = $this->schema->updateSchema($tables);
+
+        //  Any changes here should refresh cached schema
+        $this->refreshCachedTables();
+
+        if ($return_schema) {
+            return $this->describeTables($tables);
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createTable($table, $properties = [], $check_exist = false, $return_schema = false)
+    {
+        $properties = (is_array($properties) ? $properties : []);
+        $properties['name'] = $table;
+
+        $tables = static::validateAsArray($properties, null, true, 'Bad data format in request.');
+        $result = $this->schema->updateSchema($tables);
+        $result = array_get($result, 0, []);
 
         //  Any changes here should refresh cached schema
         $this->refreshCachedTables();
@@ -92,23 +119,103 @@ class Schema extends BaseNoSqlDbSchemaResource
             return $this->describeTable($table);
         }
 
-        return ['name' => $table];
+        return $result;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function updateTable($table, $properties = [], $allow_delete_fields = false, $return_schema = false)
+    public function createField($table, $field, $properties = [], $check_exist = false, $return_schema = false)
     {
-        if (empty($table)) {
-            throw new BadRequestException("No 'name' field in data.");
-        }
+        $properties = (is_array($properties) ? $properties : []);
+        $properties['name'] = $field;
 
-        $this->selectTable($table);
+        $fields = static::validateAsArray($properties, null, true, 'Bad data format in request.');
+
+        $result = $this->schema->updateFields($table, $fields);
+
+        //  Any changes here should refresh cached schema
         $this->refreshCachedTables();
 
-//		throw new InternalServerErrorException( "Failed to update table '$name'." );
-        return ['name' => $table];
+        if ($return_schema) {
+            return $this->describeField($table, $field);
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateTables($tables, $allow_delete_fields = false, $return_schema = false)
+    {
+        $tables = static::validateAsArray($tables, null, true, 'There are no table sets in the request.');
+
+        foreach ($tables as $table) {
+            if (null === ($name = array_get($table, 'name'))) {
+                throw new BadRequestException("Table schema received does not have a valid name.");
+            }
+        }
+
+        $result = $this->schema->updateSchema($tables, true, $allow_delete_fields);
+
+        //  Any changes here should refresh cached schema
+        $this->refreshCachedTables();
+
+        if ($return_schema) {
+            return $this->describeTables($tables);
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateTable($table, $properties, $allow_delete_fields = false, $return_schema = false)
+    {
+        $properties = (is_array($properties) ? $properties : []);
+        $properties['name'] = $table;
+
+        $tables = static::validateAsArray($properties, null, true, 'Bad data format in request.');
+
+        $result = $this->schema->updateSchema($tables, true, $allow_delete_fields);
+        $result = array_get($result, 0, []);
+
+        //  Any changes here should refresh cached schema
+        $this->refreshCachedTables();
+
+        if ($return_schema) {
+            return $this->describeTable($table);
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateField($table, $field, $properties = [], $allow_delete_parts = false, $return_schema = false)
+    {
+        if (empty($table)) {
+            throw new BadRequestException('Table name can not be empty.');
+        }
+
+        $properties = (is_array($properties) ? $properties : []);
+        $properties['name'] = $field;
+
+        $fields = static::validateAsArray($properties, null, true, 'Bad data format in request.');
+
+        $result = $this->schema->updateFields($table, $fields, true);
+
+        //  Any changes here should refresh cached schema
+        $this->refreshCachedTables();
+
+        if ($return_schema) {
+            return $this->describeField($table, $field);
+        }
+
+        return $result;
     }
 
     /**
@@ -116,18 +223,116 @@ class Schema extends BaseNoSqlDbSchemaResource
      */
     public function deleteTable($table, $check_empty = false)
     {
-        $name = (is_array($table)) ? array_get($table, 'name') : $table;
-        if (empty($name)) {
+        if (empty($table)) {
             throw new BadRequestException('Table name can not be empty.');
         }
 
-        try {
-            $this->selectTable($table)->drop();
-            $this->refreshCachedTables();
-
-            return ['name' => $name];
-        } catch (\Exception $ex) {
-            throw new InternalServerErrorException("Failed to delete table '$name'.\n{$ex->getMessage()}");
+        //  Does it exist
+        if (!$this->doesTableExist($table)) {
+            throw new NotFoundException("Table '$table' not found.");
         }
+
+        try {
+            $this->schema->dropTable($table);
+        } catch (\Exception $ex) {
+            \Log::error('Exception dropping table: ' . $ex->getMessage());
+
+            throw $ex;
+        }
+
+        //  Any changes here should refresh cached schema
+        $this->refreshCachedTables();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteField($table, $field)
+    {
+        if (empty($table)) {
+            throw new BadRequestException('Table name can not be empty.');
+        }
+
+        // does it already exist
+        if (!$this->doesTableExist($table)) {
+            throw new NotFoundException("A table with name '$table' does not exist in the database.");
+        }
+
+        try {
+            $this->schema->dropColumn($table, $field);
+        } catch (\Exception $ex) {
+            error_log($ex->getMessage());
+            throw $ex;
+        }
+
+        //  Any changes here should refresh cached schema
+        $this->refreshCachedTables();
+    }
+
+    /**
+     * @param string $name
+     *
+     * @throws BadRequestException
+     * @throws NotFoundException
+     * @return string
+     */
+    public function correctTableName(&$name)
+    {
+        if (false !== ($table = $this->doesTableExist($name, true))) {
+            $name = $table;
+        } else {
+            throw new NotFoundException('Table "' . $name . '" does not exist in the database.');
+        }
+    }
+
+    /**
+     * @param string $name       The name of the table to check
+     * @param bool   $returnName If true, the table name is returned instead of TRUE
+     *
+     * @throws \InvalidArgumentException
+     * @return bool
+     */
+    public function doesTableExist($name, $returnName = false)
+    {
+        return $this->schema->doesTableExist($name, $returnName);
+    }
+
+    /**
+     * @param string                $table_name
+     * @param null | string | array $field_names
+     * @param bool                  $refresh
+     *
+     * @throws NotFoundException
+     * @throws InternalServerErrorException
+     * @return array
+     */
+    public function describeTableFields($table_name, $field_names = null, $refresh = false)
+    {
+        $table = $this->schema->getTable($table_name, $refresh);
+        if (!$table) {
+            throw new NotFoundException("Table '$table_name' does not exist in the database.");
+        }
+
+        if (!empty($field_names)) {
+            $field_names = static::validateAsArray($field_names, ',', true, 'No valid field names given.');
+        }
+
+        $out = [];
+        try {
+            /** @var ColumnSchema $column */
+            foreach ($table->columns as $column) {
+                if (empty($field_names) || (false !== array_search($column->name, $field_names))) {
+                    $out[] = $column->toArray();
+                }
+            }
+        } catch (\Exception $ex) {
+            throw new InternalServerErrorException("Failed to query table field schema.\n{$ex->getMessage()}");
+        }
+
+        if (empty($out)) {
+            throw new NotFoundException("No requested fields found in table '$table_name'.");
+        }
+
+        return $out;
     }
 }
