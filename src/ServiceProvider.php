9cc8c5c9e53c0a27e54b5adc1bfefc8afedc024c
Lee Hicks <leehicks@dreamfactory.com>
2016-08-19 16:03:45 -0400

    DF-607 #resolve #comment Making service docs always viewable, even auto-generated ones.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace DreamFactory\Core\MongoDb;
 
+use DreamFactory\Core\Components\ServiceDocBuilder;
 use DreamFactory\Core\Enums\ServiceTypeGroups;
 use DreamFactory\Core\MongoDb\Models\MongoDbConfig;
 use DreamFactory\Core\MongoDb\Services\MongoDb;
@@ -10,20 +11,25 @@ use Jenssegers\Mongodb\MongodbServiceProvider;
 
 class ServiceProvider extends MongodbServiceProvider
 {
+    use ServiceDocBuilder;
+
     public function register()
     {
         parent::register();
 
         // Add our service types.
-        $this->app->resolving('df.service', function (ServiceManager $df){
+        $this->app->resolving('df.service', function (ServiceManager $df) {
             $df->addType(
                 new ServiceType([
-                    'name'           => 'mongodb',
-                    'label'          => 'MongoDB',
-                    'description'    => 'Database service for MongoDB connections.',
-                    'group'          => ServiceTypeGroups::DATABASE,
-                    'config_handler' => MongoDbConfig::class,
-                    'factory'        => function ($config){
+                    'name'            => 'mongodb',
+                    'label'           => 'MongoDB',
+                    'description'     => 'Database service for MongoDB connections.',
+                    'group'           => ServiceTypeGroups::DATABASE,
+                    'config_handler'  => MongoDbConfig::class,
+                    'default_api_doc' => function ($service) {
+                        return $this->buildServiceDoc($service->id, MongoDb::getApiDocInfo($service));
+                    },
+                    'factory'         => function ($config) {
                         return new MongoDb($config);
                     },
                 ])
